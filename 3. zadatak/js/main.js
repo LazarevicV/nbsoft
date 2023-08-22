243b30a7186f39610183000b606ee5040632089f
LazarevicV <vukadinlazarevic@gmail.com>
2023-08-22 21:50:21 +0200

    3. zadatak gg

---
+++ 3. zadatak/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+  $(".your-class").slick({
+    settings: {
+      arrows: true,
+      centerMode: true,
+      centerPadding: "40px",
+      slidesToShow: 1,
+    },
+  });
+});
