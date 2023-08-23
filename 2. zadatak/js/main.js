8745a5e92a5dedc9009c8d0be66317fad3894e1e
LazarevicV <vukadinlazarevic@gmail.com>
2023-08-23 04:35:04 +0200

    ispravljena greska u drugom zadatku sa godinom

--- 2. zadatak/js/main.js
+++ 2. zadatak/js/main.js
@@ -1,11 +1,26 @@
 $("#posalji").click(function (event) {
   event.preventDefault();
-  let data = validateData();
+  let recieved_data = validateData();
+
+  // Check if all inputs are valid before submitting the form
+  if (
+    !recieved_data.ime ||
+    !recieved_data.prezime ||
+    !recieved_data.pol ||
+    !recieved_data.godinaRodjenja ||
+    !recieved_data.grad ||
+    !recieved_data.adresa ||
+    !recieved_data.prihvati
+  ) {
+    console.log("Form data is not valid.");
+    return; // Stop form submission
+  }
+
   $.ajax({
     url: "insertIntoDB.php",
     type: "POST",
     dataType: "JSON",
-    data: data,
+    data: recieved_data,
     success: function (result) {
       let forma = $("#forma");
       forma.hide();
--- 2. zadatak/js/validation.js
+++ 2. zadatak/js/validation.js
@@ -9,7 +9,10 @@ function validateGender(gender) {
 function validateBirthYear(year) {
   const currentYear = new Date().getFullYear();
   const parsedYear = parseInt(year);
-  return !isNaN(parsedYear) && parsedYear >= 1900 && parsedYear <= currentYear;
+  const isValid =
+    !isNaN(parsedYear) && parsedYear >= 1900 && parsedYear <= currentYear;
+
+  return isValid;
 }
 
 function validateCity(city) {
@@ -32,26 +35,37 @@ function validateData() {
   // remove all is-invalid classes if they exist
   $(".is-invalid").removeClass("is-invalid");
 
+  // remove all error messages if they exist
+  let errorSpans = $("span[id*='Greska']");
+  errorSpans.text("");
+
   if (!validateName(ime)) {
     $("#ime").addClass("is-invalid");
+    $("#imeGreska").text("Ime nije validno.");
   }
   if (!validateName(prezime)) {
     $("#prezime").addClass("is-invalid");
+    $("#prezimeGreska").text("Prezime nije validno.");
   }
   if (!validateGender(pol)) {
     $("#pol").addClass("is-invalid");
+    $("#polGreska").text("Pol nije validan.");
   }
   if (!validateBirthYear(godinaRodjenja)) {
     $("#godinaRodjenja").addClass("is-invalid");
+    $("#godinaRodjenjaGreska").text("Godina rodjenja nije validna.");
   }
   if (!validateCity(grad)) {
     $("#grad").addClass("is-invalid");
+    $("#gradGreska").text("Grad nije validan.");
   }
   if (!validateAddress(adresa)) {
     $("#adresa").addClass("is-invalid");
+    $("#adresaGreska").text("Adresa nije validna.");
   }
   if (!prihvati) {
     $("#prihvati").addClass("is-invalid");
+    $("#prihvatiGreska").text("Morate prihvatiti uslove koriscenja!");
   }
 
   data = {
