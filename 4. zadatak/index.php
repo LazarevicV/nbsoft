fc1038b0da9a87f2beb005d141141f53f2418dc1
LazarevicV <vukadinlazarevic@gmail.com>
2023-08-25 14:43:35 +0200

    deleted comments, fixed 404 redirect

--- 4. zadatak/index.php
+++ 4. zadatak/index.php
@@ -1,10 +1,5 @@
 <?php
 require_once './connection/connection.php';
-// require_once './controllers/Controller.php';
-// require_once './controllers/ApiController.php';
-// require_once './controllers/RedirectController.php';
-// require_once './controllers/UserController.php';
-// require_once './controllers/ViewController.php';
 
 // we take request and method from the server
 // and we remove the prefix from the request
@@ -13,12 +8,6 @@ $prefix = '/nbsoft/4.%20zadatak';
 $request = str_replace($prefix, '', $request);
 $method = $_SERVER['REQUEST_METHOD'];
 
-// initialize controllers
-// $controller = new Controller($conn);
-// $apiController = new ApiController($conn);
-// $redirectController = new RedirectController($conn);
-// $userController = new UserController($conn);
-// $viewController = new ViewController($conn);
 
 $ROUTES = [
     "GET" => [
@@ -51,48 +40,8 @@ if (isset($ROUTES[$method][$request])) {
     $controllerInstance = new $controllerClassName($conn);
     $controllerInstance->$method();
 } else {
+    // If the route is not found, redirect to 404
+    require_once './controllers/RedirectController.php';
+    $redirectController = new RedirectController($conn);
     $redirectController->error404();
 }
-
-// check if there is get method for certain route
-// if ($method === 'GET') {
-//     if ($request === '' || $request === '/') {
-//         // this is the home page of the application
-//         $viewController->homePage();
-//     } else if ($request === '/api/users') {
-//         $apiController->users();
-//     }
-//     // provera da li postoji get metod ka sledecoj ruti
-//     // ako postoji onda prikazuje json sadrzaj podataka iz baze
-//     else if ($request === '/api/orders') {
-//         $apiController->orders();
-//     } else if ($request === '/register') {
-//         session_start();
-//         if (isset($_SESSION['user'])) { // we check if there is logged user
-//             if ($_SESSION['user']->role == "admin") { // and if his role is admin
-//                 $viewController->register(); // if its admin we can show him register page
-//             } else {
-//                 $redirectController->redirectHome(); // if its not admin we redirect him to home page
-//             }
-//         } else {
-//             $redirectController->redirectLogin(); // same if the user is not logged in, but on login page
-//         }
-//     } else if ($request === '/page-not-found') {
-//         $viewController->pageNotFound();
-//     } else if ($request === '/login') {
-//         $viewController->login();
-//     } else if ($request === '/logout') {
-//         $userController->logout();
-//     } else {
-//         $redirectController->error404();
-//     }
-// } else { // if there is no get method, it means that there is post method
-//     if ($request === '/check_login') {
-//         $userController->check_login();
-//     }
-//     if ($request === '/process_registration') {
-//         $userController->process_registration();
-//     } else {
-//         $redirectController->error404();
-//     }
-// }
