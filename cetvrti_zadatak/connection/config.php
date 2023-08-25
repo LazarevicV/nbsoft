de4481afc3ad600d3d4ea994a0b460dcb2a5b391
LazarevicV <vukadinlazarevic@gmail.com>
2023-08-25 22:01:26 +0200

    changed default path

--- cetvrti_zadatak/connection/config.php
+++ cetvrti_zadatak/connection/config.php
@@ -9,8 +9,8 @@ define("SERVERNAME", "localhost");
 define("USERNAME", "root");
 define("PASSWORD", "");
 define("DBNAME", "nbsoft-4zadatak");
-define("PREFIX", "/zadaci/nbsoft/cetvrti_zadatak"); //if app is in
+define("PREFIX", "/nbsoft/cetvrti_zadatak"); //if app is in
 // subfolder.in that case /subfolder/
 // define("PREFIX", "/subfolder/nbsoft/4.%20zadatak");
-define("PREFIX_HOME", "http://localhost/zadaci/nbsoft/cetvrti_zadatak");
+define("PREFIX_HOME", "http://localhost/nbsoft/cetvrti_zadatak");
 // define("PREFIX_HOME", "http://localhost/subfoler/nbsoft/4.%20zadatak");
--- peti_zadatak/connection/config.php
+++ peti_zadatak/connection/config.php
@@ -3,4 +3,5 @@ define("SERVERNAME", "localhost");
 define("USERNAME", "root");
 define("PASSWORD", "");
 define("DBNAME", "nbsoft-4zadatak");
-define("PREFIX", "http://localhost/zadaci/nbsoft/peti_zadatak");
+define("PREFIX", "http://localhost/nbsoft/peti_zadatak");
+// define("PREFIX", "http://localhost/subfolder/nbsoft/peti_zadatak");
