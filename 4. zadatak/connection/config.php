76a88d1a117ea9b5caaca455646cd307a48e9456
LazarevicV <vukadinlazarevic@gmail.com>
2023-08-25 18:56:14 +0200

    added comments and changed text in index.php in first assigment

--- 4. zadatak/connection/config.php
+++ 4. zadatak/connection/config.php
@@ -1,7 +1,15 @@
 <?php
+
+/**
+ * Global configuration
+ */
+
+// localhost
 define("SERVERNAME", "localhost");
 define("USERNAME", "root");
 define("PASSWORD", "");
 define("DBNAME", "nbsoft-4zadatak");
-define("PREFIX", "/nbsoft/4.%20zadatak");
+define("PREFIX", "/nbsoft/4.%20zadatak"); //if app is in subfolder. in that case /subfolder/
+// define("PREFIX", "/subfolder/nbsoft/4.%20zadatak");
 define("PREFIX_HOME", "http://localhost/nbsoft/4.%20zadatak");
+// define("PREFIX_HOME", "http://localhost/subfoler/nbsoft/4.%20zadatak");
