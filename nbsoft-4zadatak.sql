b01e15068a5357d492c13dc9ff7b61c6efeebba0
LazarevicV <vukadinlazarevic@gmail.com>
2023-08-25 02:28:04 +0200

    added databases

---
+++ nbsoft-4zadatak.sql
@@ -0,0 +1,202 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 25, 2023 at 02:27 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nbsoft-4zadatak`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orderItems`
+--
+
+CREATE TABLE `orderItems` (
+  `id` int(11) NOT NULL,
+  `orderId` int(11) NOT NULL,
+  `value` float NOT NULL,
+  `productId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orderItems`
+--
+
+INSERT INTO `orderItems` (`id`, `orderId`, `value`, `productId`) VALUES
+(1, 1, 3, 1),
+(2, 1, 2, 1),
+(3, 2, 5, 2),
+(4, 2, 2, 3),
+(5, 1, 2, 3),
+(6, 1, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `orders`
+--
+
+CREATE TABLE `orders` (
+  `id` int(11) NOT NULL,
+  `userId` int(11) NOT NULL,
+  `value` float DEFAULT NULL,
+  `dateCreate` datetime NOT NULL DEFAULT current_timestamp(),
+  `dateEdit` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `userId`, `value`, `dateCreate`, `dateEdit`) VALUES
+(1, 1, NULL, '2023-08-24 22:40:47', '2023-08-24 22:40:41'),
+(2, 4, NULL, '2023-08-25 00:50:23', NULL),
+(4, 1, NULL, '2023-08-25 01:33:26', '2023-08-25 01:33:23');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Product`
+--
+
+CREATE TABLE `Product` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `price` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `Product`
+--
+
+INSERT INTO `Product` (`id`, `name`, `price`) VALUES
+(1, 'Banana', 149.99),
+(2, 'Cips', 49.99),
+(3, 'Jabuke', 99.99);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `ime` varchar(50) NOT NULL,
+  `prezime` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `telefon` varchar(10) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `grad` varchar(50) NOT NULL,
+  `postanski_broj` varchar(10) NOT NULL,
+  `adresa` varchar(50) NOT NULL,
+  `role` varchar(10) NOT NULL DEFAULT 'user',
+  `last_login` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `ime`, `prezime`, `email`, `telefon`, `username`, `password`, `grad`, `postanski_broj`, `adresa`, `role`, `last_login`) VALUES
+(1, 'Vukadin', 'Lazarevic', 'vlazarevic@raf.rs', '0603413252', 'vlazarevic', 'd2a8d138cd31136f906fca85e81986d60df9a97c2551c63144ac4d370da38622', 'Beograd', '11130', 'Kozaracka 15', 'admin', '2023-08-24 23:46:29'),
+(2, 'Marko', 'Bozic', 'mbozic@raf.rs', '060123456', 'mbozic', '406892809c1d9de3b80b9ba86f7b332c6cd8f8d131d69bb1c3f0ab5c9b668b5f', 'Beograd', '11000', 'Usce', 'user', '2023-08-24 22:42:45'),
+(4, 'Ana', 'Rajkovic', 'anarajkovic101@gmail.com', '060123456', 'arajkovic', '2dd591b21d63deb5c0ddef813831f7d09e82d0fa8f9d385ef0b82fd8bf9cef0a', 'Beograd', '11130', 'Branka Copica 32', 'user', '2023-08-24 23:47:18');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `orderItems`
+--
+ALTER TABLE `orderItems`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `orderId` (`orderId`),
+  ADD KEY `productId` (`productId`);
+
+--
+-- Indexes for table `orders`
+--
+ALTER TABLE `orders`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `userId` (`userId`);
+
+--
+-- Indexes for table `Product`
+--
+ALTER TABLE `Product`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `orderItems`
+--
+ALTER TABLE `orderItems`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `orders`
+--
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `Product`
+--
+ALTER TABLE `Product`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `orderItems`
+--
+ALTER TABLE `orderItems`
+  ADD CONSTRAINT `orderitems_ibfk_1` FOREIGN KEY (`orderId`) REFERENCES `orders` (`id`),
+  ADD CONSTRAINT `orderitems_ibfk_2` FOREIGN KEY (`productId`) REFERENCES `Product` (`id`);
+
+--
+-- Constraints for table `orders`
+--
+ALTER TABLE `orders`
+  ADD CONSTRAINT `orders_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ nbsoft_2zadatak.sql
@@ -0,0 +1,83 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 25, 2023 at 02:27 AM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 7.3.33
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `nbsoft_2zadatak`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `User`
+--
+
+CREATE TABLE `User` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `pol` varchar(255) NOT NULL,
+  `godinaRodjenja` int(11) NOT NULL,
+  `grad` varchar(255) NOT NULL,
+  `adresa` varchar(255) NOT NULL,
+  `prihvati` tinyint(4) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `User`
+--
+
+INSERT INTO `User` (`id`, `firstName`, `lastName`, `pol`, `godinaRodjenja`, `grad`, `adresa`, `prihvati`) VALUES
+(1, 'Vukadin', 'Lazarevic', 'muski', 2002, 'Beograd', 'Kozaracka 15', 0),
+(2, 'David', 'Stevic', 'drugo', 2010, 'NS', 'Kod pajica', 1),
+(3, 'Ana', 'Rajkovic', 'zenski', 2003, 'Beograd', 'Branka Copica 32', 0),
+(4, 'Filip', 'Pajic', 'drugo', 1990, 'NS', 'Kod Milice', 1),
+(5, 'Marko', 'Lazarevic', 'muski', 2002, 'Beograd', 'Kozaracka 15', 1),
+(6, 'Vukadin', 'Lazarevic', 'muski', 1234, 'Beograd', '123123312', 1),
+(7, 'Jovan', 'Lazarevic', 'zenski', 2007, 'Beograd', 'Kozaracka 15', 0),
+(8, 'Marko', 'Bozic', 'muski', 2001, 'Beograd', 'Usce', 1),
+(10, 'Vukadin', 'Lazarevic', 'muski', 2002, 'Beograd', 'Kozaracka 15', 1),
+(11, 'Pera', 'Peric', 'zenski', 1999, 'Beograd', 'Kozaracka 15', 1),
+(12, 'Marko', 'Bozic', 'muski', 2001, 'Beograd', 'usce', 1),
+(22, 'Vukadin', 'Lazarevic', 'muski', 2010, 'Beograd', 'Kozaracka 15', 1),
+(23, 'Vukadin', 'Lazarevic', 'muski', 1999, 'Beograd', 'Kozaracka 15', 1);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `User`
+--
+ALTER TABLE `User`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `User`
+--
+ALTER TABLE `User`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
