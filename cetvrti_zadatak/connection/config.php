3c93d61337029f0a59673e05119cf64dc59ed9a0
LazarevicV <vukadinlazarevic@gmail.com>
2023-08-26 03:16:01 +0200

    changed commentar

--- cetvrti_zadatak/connection/config.php
+++ cetvrti_zadatak/connection/config.php
@@ -11,6 +11,6 @@ define("PASSWORD", "");
 define("DBNAME", "nbsoft-4zadatak");
 define("PREFIX", "/nbsoft/cetvrti_zadatak"); //if app is in
 // subfolder.in that case /subfolder/
-// define("PREFIX", "/subfolder/nbsoft/4.%20zadatak");
+// define("PREFIX", "/subfolder/nbsoft/cetvrti_zadatak");
 define("PREFIX_HOME", "http://localhost/nbsoft/cetvrti_zadatak");
-// define("PREFIX_HOME", "http://localhost/subfoler/nbsoft/4.%20zadatak");
+// define("PREFIX_HOME", "http://localhost/subfoler/nbsoft/cetvrti_zadatak");
